fc53c1580d1efb2ef8e4ce5e0f46171f43af0f0b
audsssy <stamfordh@hotmail.com>
2024-11-25 12:53:26 -0800

    🧘 cleanup

--- src/tokens/g0v/HackathonSupportToken.sol
+++ src/tokens/g0v/HackathonSupportToken.sol
@@ -7,191 +7,173 @@ import {ERC721} from "solbase/tokens/ERC721/ERC721.sol";
 
 /// @title Impact NFTs
 /// @notice SVG NFTs displaying impact results and metrics.
-contract HackathonSupportToken is ERC721 {
-    /// -----------------------------------------------------------------------
-    /// Storage
-    /// -----------------------------------------------------------------------
-
-    address public immutable quest;
-    address public immutable mission;
-    uint256 public missionId;
-    uint256 public taskId;
-    address public immutable curve;
-    uint256 public totalSupply;
-
-    /// -----------------------------------------------------------------------
-    /// Constructor & Modifier
-    /// -----------------------------------------------------------------------
-
-    constructor(string memory _name, string memory _symbol, address _quest, address _mission, address _curve)
-        ERC721(_name, _symbol)
-    {
-        quest = _quest;
-        mission = _mission;
-        curve = _curve;
-    }
-
-    modifier onlyCurve() {
-        if (msg.sender != curve) revert Unauthorized();
-        _;
-    }
-
-    modifier onlyOwnerOrCurve(uint256 id) {
-        if (msg.sender != ownerOf(id) && msg.sender != curve) revert Unauthorized();
-        _;
-    }
-
-    /// -----------------------------------------------------------------------
-    /// Mint / Burn Logic
-    /// -----------------------------------------------------------------------
-
-    function mint(address to) external payable onlyCurve {
-        unchecked {
-            ++totalSupply;
-        }
-
-        _safeMint(to, totalSupply);
-    }
-
-    function burn(uint256 id) external payable onlyOwnerOrCurve(id) {
-        unchecked {
-            --totalSupply;
-        }
-
-        _burn(id);
-    }
-
-    /// -----------------------------------------------------------------------
-    /// SVG Inputs
-    /// -----------------------------------------------------------------------
-
-    function setSvgInputs(uint256 _missionId, uint256 _taskId) external payable {
-        missionId = _missionId;
-        taskId = _taskId;
-    }
-
-    /// -----------------------------------------------------------------------
-    /// Metadata Storage & Logic
-    /// -----------------------------------------------------------------------
-
-    function tokenURI(uint256 id) public view override returns (string memory) {
-        return _buildURI(id);
-    }
-
-    // credit: z0r0z.eth (https://github.com/kalidao/kali-contracts/blob/60ba3992fb8d6be6c09eeb74e8ff3086a8fdac13/contracts/access/KaliAccessManager.sol)
-    function _buildURI(uint256 id) private view returns (string memory) {
-        return JSON._formattedMetadata("g0v Hackathon Support Token", "", generateSvg(id));
-    }
-
-    function generateSvg(uint256 id) public view returns (string memory) {
-        return string.concat(
-            '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" style="background:#FFFBF5">',
-            SVG._text(
-                string.concat(
-                    SVG._prop("x", "20"),
-                    SVG._prop("y", "40"),
-                    SVG._prop("font-size", "20"),
-                    SVG._prop("fill", "#00040a")
-                ),
-                string.concat(unicode"沒有人 #", SVG._uint2str(id))
-            ),
-            SVG._rect(
-                string.concat(
-                    SVG._prop("fill", "#FFBE0B"),
-                    SVG._prop("x", "20"),
-                    SVG._prop("y", "50"),
-                    SVG._prop("width", SVG._uint2str(160)),
-                    SVG._prop("height", SVG._uint2str(5))
-                ),
-                SVG.NULL
-            ),
-            // buildTaskChart(),
-            buildSvgData(),
-            "</svg>"
-        );
-    }
-
-    function buildSvgData() public view returns (string memory) {
-        // The number of hackath0ns hosted by g0v.
-        // uint256 hackathonCount = 59 + IMission(mission).getMissionTaskCount(missionId);
-
-        return string.concat(
-            SVG._text(
-                string.concat(
-                    SVG._prop("x", "20"),
-                    SVG._prop("y", "100"),
-                    SVG._prop("font-size", "20"),
-                    SVG._prop("fill", "#00040a")
-                ),
-                "IMission(mission).getMissionTitle(missionId)"
-            ),
-            SVG._text(
-                string.concat(
-                    SVG._prop("x", "20"),
-                    SVG._prop("y", "230"),
-                    SVG._prop("font-size", "12"),
-                    SVG._prop("fill", "#00040a")
-                ),
-                string.concat(
-                    unicode"n0body 參與人數：",
-                    // SVG._uint2str(IQuest(quest).getNumOfStartsByMissionByPublic(mission, missionId)),
-                    unicode" 人"
-                )
-            ),
-            SVG._text(
-                string.concat(
-                    SVG._prop("x", "20"),
-                    SVG._prop("y", "250"),
-                    SVG._prop("font-size", "12"),
-                    SVG._prop("fill", "#00040a")
-                ),
-                string.concat(
-                    unicode"總參與人數：",
-                    "SVG._uint2str(IMission(mission).getMissionStarts(missionId))",
-                    unicode" 人"
-                )
-            ),
-            SVG._text(
-                string.concat(
-                    SVG._prop("x", "20"),
-                    SVG._prop("y", "270"),
-                    SVG._prop("font-size", "12"),
-                    SVG._prop("fill", "#00040a")
-                ),
-                string.concat(
-                    unicode"100% 參與人數：",
-                    "SVG._uint2str(IMission(mission).getMissionCompletions(missionId))",
-                    unicode" 人"
-                )
-            ),
-            SVG._text(
-                string.concat(
-                    SVG._prop("x", "20"),
-                    SVG._prop("y", "170"),
-                    SVG._prop("font-size", "12"),
-                    SVG._prop("fill", "#00040a")
-                ),
-                ""
-            ),
-            // string.concat(unicode"第 ", SVG._uint2str(hackathonCount), unicode" 次參與人數：")
-            SVG._text(
-                string.concat(
-                    SVG._prop("x", "140"),
-                    SVG._prop("y", "170"),
-                    SVG._prop("font-size", "40"),
-                    SVG._prop("fill", "#00040a")
-                ),
-                "SVG._uint2str(IMission(mission).getTotalTaskCompletionsByMission(missionId, taskId))"
-            ),
-            SVG._text(
-                string.concat(
-                    SVG._prop("x", "230"),
-                    SVG._prop("y", "170"),
-                    SVG._prop("font-size", "11"),
-                    SVG._prop("fill", "#00040a")
-                ),
-                unicode" 人"
-            )
-        );
-    }
+abstract contract HackathonSupportToken is ERC721 {
+    // /// -----------------------------------------------------------------------
+    // /// Storage
+    // /// -----------------------------------------------------------------------
+    // address public immutable quest;
+    // address public immutable mission;
+    // uint256 public missionId;
+    // uint256 public taskId;
+    // address public immutable curve;
+    // uint256 public totalSupply;
+    // /// -----------------------------------------------------------------------
+    // /// Constructor & Modifier
+    // /// -----------------------------------------------------------------------
+    // constructor(string memory _name, string memory _symbol, address _quest, address _mission, address _curve)
+    //     ERC721(_name, _symbol)
+    // {
+    //     quest = _quest;
+    //     mission = _mission;
+    //     curve = _curve;
+    // }
+    // modifier onlyCurve() {
+    //     if (msg.sender != curve) revert Unauthorized();
+    //     _;
+    // }
+    // modifier onlyOwnerOrCurve(uint256 id) {
+    //     if (msg.sender != ownerOf(id) && msg.sender != curve) revert Unauthorized();
+    //     _;
+    // }
+    // /// -----------------------------------------------------------------------
+    // /// Mint / Burn Logic
+    // /// -----------------------------------------------------------------------
+    // function mint(address to) external payable onlyCurve {
+    //     unchecked {
+    //         ++totalSupply;
+    //     }
+    //     _safeMint(to, totalSupply);
+    // }
+    // function burn(uint256 id) external payable onlyOwnerOrCurve(id) {
+    //     unchecked {
+    //         --totalSupply;
+    //     }
+    //     _burn(id);
+    // }
+    // /// -----------------------------------------------------------------------
+    // /// SVG Inputs
+    // /// -----------------------------------------------------------------------
+    // function setSvgInputs(uint256 _missionId, uint256 _taskId) external payable {
+    //     missionId = _missionId;
+    //     taskId = _taskId;
+    // }
+    // /// -----------------------------------------------------------------------
+    // /// Metadata Storage & Logic
+    // /// -----------------------------------------------------------------------
+    // function tokenURI(uint256 id) public view override returns (string memory) {
+    //     return _buildURI(id);
+    // }
+    // // credit: z0r0z.eth (https://github.com/kalidao/kali-contracts/blob/60ba3992fb8d6be6c09eeb74e8ff3086a8fdac13/contracts/access/KaliAccessManager.sol)
+    // function _buildURI(uint256 id) private view returns (string memory) {
+    //     return JSON._formattedMetadata("g0v Hackathon Support Token", "", generateSvg(id));
+    // }
+    // function generateSvg(uint256 id) public view returns (string memory) {
+    //     return string.concat(
+    //         '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" style="background:#FFFBF5">',
+    //         SVG._text(
+    //             string.concat(
+    //                 SVG._prop("x", "20"),
+    //                 SVG._prop("y", "40"),
+    //                 SVG._prop("font-size", "20"),
+    //                 SVG._prop("fill", "#00040a")
+    //             ),
+    //             string.concat(unicode"沒有人 #", SVG._uint2str(id))
+    //         ),
+    //         SVG._rect(
+    //             string.concat(
+    //                 SVG._prop("fill", "#FFBE0B"),
+    //                 SVG._prop("x", "20"),
+    //                 SVG._prop("y", "50"),
+    //                 SVG._prop("width", SVG._uint2str(160)),
+    //                 SVG._prop("height", SVG._uint2str(5))
+    //             ),
+    //             SVG.NULL
+    //         ),
+    //         // buildTaskChart(),
+    //         buildSvgData(),
+    //         "</svg>"
+    //     );
+    // }
+    // function buildSvgData() public view returns (string memory) {
+    //     // The number of hackath0ns hosted by g0v.
+    //     // uint256 hackathonCount = 59 + IMission(mission).getMissionTaskCount(missionId);
+    //     return string.concat(
+    //         SVG._text(
+    //             string.concat(
+    //                 SVG._prop("x", "20"),
+    //                 SVG._prop("y", "100"),
+    //                 SVG._prop("font-size", "20"),
+    //                 SVG._prop("fill", "#00040a")
+    //             ),
+    //             "IMission(mission).getMissionTitle(missionId)"
+    //         ),
+    //         SVG._text(
+    //             string.concat(
+    //                 SVG._prop("x", "20"),
+    //                 SVG._prop("y", "230"),
+    //                 SVG._prop("font-size", "12"),
+    //                 SVG._prop("fill", "#00040a")
+    //             ),
+    //             string.concat(
+    //                 unicode"n0body 參與人數：",
+    //                 // SVG._uint2str(IQuest(quest).getNumOfStartsByMissionByPublic(mission, missionId)),
+    //                 unicode" 人"
+    //             )
+    //         ),
+    //         SVG._text(
+    //             string.concat(
+    //                 SVG._prop("x", "20"),
+    //                 SVG._prop("y", "250"),
+    //                 SVG._prop("font-size", "12"),
+    //                 SVG._prop("fill", "#00040a")
+    //             ),
+    //             string.concat(
+    //                 unicode"總參與人數：",
+    //                 "SVG._uint2str(IMission(mission).getMissionStarts(missionId))",
+    //                 unicode" 人"
+    //             )
+    //         ),
+    //         SVG._text(
+    //             string.concat(
+    //                 SVG._prop("x", "20"),
+    //                 SVG._prop("y", "270"),
+    //                 SVG._prop("font-size", "12"),
+    //                 SVG._prop("fill", "#00040a")
+    //             ),
+    //             string.concat(
+    //                 unicode"100% 參與人數：",
+    //                 "SVG._uint2str(IMission(mission).getMissionCompletions(missionId))",
+    //                 unicode" 人"
+    //             )
+    //         ),
+    //         SVG._text(
+    //             string.concat(
+    //                 SVG._prop("x", "20"),
+    //                 SVG._prop("y", "170"),
+    //                 SVG._prop("font-size", "12"),
+    //                 SVG._prop("fill", "#00040a")
+    //             ),
+    //             ""
+    //         ),
+    //         // string.concat(unicode"第 ", SVG._uint2str(hackathonCount), unicode" 次參與人數：")
+    //         SVG._text(
+    //             string.concat(
+    //                 SVG._prop("x", "140"),
+    //                 SVG._prop("y", "170"),
+    //                 SVG._prop("font-size", "40"),
+    //                 SVG._prop("fill", "#00040a")
+    //             ),
+    //             "SVG._uint2str(IMission(mission).getTotalTaskCompletionsByMission(missionId, taskId))"
+    //         ),
+    //         SVG._text(
+    //             string.concat(
+    //                 SVG._prop("x", "230"),
+    //                 SVG._prop("y", "170"),
+    //                 SVG._prop("font-size", "11"),
+    //                 SVG._prop("fill", "#00040a")
+    //             ),
+    //             unicode" 人"
+    //         )
+    //     );
+    // }
 }
