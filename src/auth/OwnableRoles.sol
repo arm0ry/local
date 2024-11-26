93779f724f634abb1c1312cc00348e417a7023a2
audsssy <stamfordh@hotmail.com>
2024-11-26 12:07:47 -0800

    Update OwnableRoles.sol

--- src/auth/OwnableRoles.sol
+++ src/auth/OwnableRoles.sol
@@ -56,7 +56,11 @@ abstract contract OwnableRoles is Ownable {
     /// @dev Updates the roles directly without authorization guard.
     /// If `on` is true, each set bit of `roles` will be turned on,
     /// otherwise, each set bit of `roles` will be turned off.
-    function _updateRoles(address user, uint256 roles, bool on) internal virtual {
+    function _updateRoles(
+        address user,
+        uint256 roles,
+        bool on
+    ) internal virtual {
         /// @solidity memory-safe-assembly
         assembly {
             mstore(0x0c, _ROLE_SLOT_SEED)
@@ -69,11 +73,19 @@ abstract contract OwnableRoles is Ownable {
             // Compute the updated roles if `on` is false.
             // Use `and` to compute the intersection of `current` and `roles`,
             // `xor` it with `current` to flip the bits in the intersection.
-            if iszero(on) { updated := xor(current, and(current, roles)) }
+            if iszero(on) {
+                updated := xor(current, and(current, roles))
+            }
             // Then, store the new value.
             sstore(roleSlot, updated)
             // Emit the {RolesUpdated} event.
-            log3(0, 0, _ROLES_UPDATED_EVENT_SIGNATURE, shr(96, mload(0x0c)), updated)
+            log3(
+                0,
+                0,
+                _ROLES_UPDATED_EVENT_SIGNATURE,
+                shr(96, mload(0x0c)),
+                updated
+            )
         }
     }
 
@@ -108,24 +120,24 @@ abstract contract OwnableRoles is Ownable {
     /// @dev Throws if the sender is not the owner,
     /// and does not have any of the `roles`.
     /// Checks for ownership first, then lazily checks for roles.
-    // function _checkOwnerOrRoles(uint256 roles) internal view virtual {
-    //     /// @solidity memory-safe-assembly
-    //     assembly {
-    //         // If the caller is not the stored owner.
-    //         // Note: `_ROLE_SLOT_SEED` is equal to `_OWNER_SLOT_NOT`.
-    //         if iszero(eq(caller(), sload(not(_ROLE_SLOT_SEED)))) {
-    //             // Compute the role slot.
-    //             mstore(0x0c, _ROLE_SLOT_SEED)
-    //             mstore(0x00, caller())
-    //             // Load the stored value, and if the `and` intersection
-    //             // of the value and `roles` is zero, revert.
-    //             if iszero(and(sload(keccak256(0x0c, 0x20)), roles)) {
-    //                 mstore(0x00, 0x82b42900) // `Unauthorized()`.
-    //                 revert(0x1c, 0x04)
-    //             }
-    //         }
-    //     }
-    // }
+    function _checkOwnerOrRoles(uint256 roles) internal view virtual {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // If the caller is not the stored owner.
+            // Note: `_ROLE_SLOT_SEED` is equal to `_OWNER_SLOT_NOT`.
+            if iszero(eq(caller(), sload(not(_ROLE_SLOT_SEED)))) {
+                // Compute the role slot.
+                mstore(0x0c, _ROLE_SLOT_SEED)
+                mstore(0x00, caller())
+                // Load the stored value, and if the `and` intersection
+                // of the value and `roles` is zero, revert.
+                if iszero(and(sload(keccak256(0x0c, 0x20)), roles)) {
+                    mstore(0x00, 0x82b42900) // `Unauthorized()`.
+                    revert(0x1c, 0x04)
+                }
+            }
+        }
+    }
 
     /// @dev Throws if the sender does not have any of the `roles`,
     /// and is not the owner.
@@ -153,10 +165,16 @@ abstract contract OwnableRoles is Ownable {
     /// This is meant for frontends like Etherscan, and is therefore not fully optimized.
     /// Not recommended to be called on-chain.
     /// Made internal to conserve bytecode. Wrap it in a public function if needed.
-    function _rolesFromOrdinals(uint8[] memory ordinals) internal pure returns (uint256 roles) {
+    function _rolesFromOrdinals(
+        uint8[] memory ordinals
+    ) internal pure returns (uint256 roles) {
         /// @solidity memory-safe-assembly
         assembly {
-            for { let i := shl(5, mload(ordinals)) } i { i := sub(i, 0x20) } {
+            for {
+                let i := shl(5, mload(ordinals))
+            } i {
+                i := sub(i, 0x20)
+            } {
                 // We don't need to mask the values of `ordinals`, as Solidity
                 // cleans dirty upper bits when storing variables into memory.
                 roles := or(shl(mload(add(ordinals, i)), 1), roles)
@@ -168,7 +186,9 @@ abstract contract OwnableRoles is Ownable {
     /// This is meant for frontends like Etherscan, and is therefore not fully optimized.
     /// Not recommended to be called on-chain.
     /// Made internal to conserve bytecode. Wrap it in a public function if needed.
-    function _ordinalsFromRoles(uint256 roles) internal pure returns (uint8[] memory ordinals) {
+    function _ordinalsFromRoles(
+        uint256 roles
+    ) internal pure returns (uint8[] memory ordinals) {
         /// @solidity memory-safe-assembly
         assembly {
             // Grab the pointer to the free memory.
@@ -177,14 +197,20 @@ abstract contract OwnableRoles is Ownable {
             let o := 0
             // The absence of lookup tables, De Bruijn, etc., here is intentional for
             // smaller bytecode, as this function is not meant to be called on-chain.
-            for { let t := roles } 1 {} {
+            for {
+                let t := roles
+            } 1 {
+
+            } {
                 mstore(ptr, o)
                 // `shr` 5 is equivalent to multiplying by 0x20.
                 // Push back into the ordinals array if the bit is set.
                 ptr := add(ptr, shl(5, and(t, 1)))
                 o := add(o, 1)
                 t := shr(o, roles)
-                if iszero(t) { break }
+                if iszero(t) {
+                    break
+                }
             }
             // Store the length of `ordinals`.
             mstore(ordinals, shr(5, sub(ptr, add(ordinals, 0x20))))
@@ -199,13 +225,19 @@ abstract contract OwnableRoles is Ownable {
 
     /// @dev Allows the owner to grant `user` `roles`.
     /// If the `user` already has a role, then it will be an no-op for the role.
-    function grantRoles(address user, uint256 roles) public payable virtual onlyOwner {
+    function grantRoles(
+        address user,
+        uint256 roles
+    ) public payable virtual onlyOwner {
         _grantRoles(user, roles);
     }
 
     /// @dev Allows the owner to remove `user` `roles`.
     /// If the `user` does not have a role, then it will be an no-op for the role.
-    function revokeRoles(address user, uint256 roles) public payable virtual onlyOwner {
+    function revokeRoles(
+        address user,
+        uint256 roles
+    ) public payable virtual onlyOwner {
         _removeRoles(user, roles);
     }
 
@@ -232,7 +264,10 @@ abstract contract OwnableRoles is Ownable {
     }
 
     /// @dev Returns whether `user` has any of `roles`.
-    function hasAnyRole(address user, uint256 roles) public view virtual returns (bool) {
+    function hasAnyRole(
+        address user,
+        uint256 roles
+    ) public view virtual returns (bool) {
         return rolesOf(user) & roles != 0;
     }
 
@@ -253,10 +288,10 @@ abstract contract OwnableRoles is Ownable {
 
     /// @dev Marks a function as only callable by the owner or by an account
     /// with `roles`. Checks for ownership first, then lazily checks for roles.
-    // modifier onlyOwnerOrRoles(uint256 roles) virtual {
-    //     _checkOwnerOrRoles(roles);
-    //     _;
-    // }
+    modifier onlyOwnerOrRoles(uint256 roles) virtual {
+        _checkOwnerOrRoles(roles);
+        _;
+    }
 
     /// @dev Marks a function as only callable by an account with `roles`
     /// or the owner. Checks for roles first, then lazily checks for ownership.
